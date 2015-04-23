10c32d886aabcb20c082992e2e1a74f9d7924449
Chen Zhu <cz764@nyu.edu>
2015-04-23 12:00:00 -0400

    remove initialize screen

--- engine.js
+++ engine.js
@@ -167,8 +167,7 @@ var SpriteSheet = new function() {
 var TitleScreen = function TitleScreen(title,subtitle,callback) {
   var up = false;
   this.step = function(dt) {
-    if(!Game.keys['fire']) up = true;
-    if(up && Game.keys['fire'] && callback) callback();
+    if(callback) callback();
   };
 
   this.draw = function(ctx) {
